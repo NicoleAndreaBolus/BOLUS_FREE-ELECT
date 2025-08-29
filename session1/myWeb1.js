cf891bdac67e65533f353e63e005b6b3109f7888
NicoleAndreaBolus <nagbolus.student@ua.edu.ph>
2025-08-29 17:58:51 +0800

    Added changes

--- session1/myWeb1.js
+++ session1/myWeb1.js
@@ -8,10 +8,24 @@ console.log($lname);
 
 // Using 'let'
 let $mySal = 25000;
-console.log($mySal);
+    $mySal = 50000; // Re-assigning a new value (this will overwrite the previous value)
+
+let $Address = "San Fernando City, Pampanga";
+
+// Using 'const'
+
+const pi = 3.1415;
+console.log(pi);
+
+// Concatenation
+let fullName = $name + " " + $lname;
+console.log("Hello, " + fullName + "!");
+console.log("Hello, " + $name + ". How are you?");
 
 // ------ Output ------
 console.log("Hello, BSIT 3B!");
 console.log("Welcome to our JavaScript Class!");
+document.getElementById("myAddress").innerHTML = $Address;
+console.log($mySal);
 
 
---
+++ session2/myWeb2.js
@@ -0,0 +1,23 @@
+
+$name = "Colin";
+$name2 = "Jayson";
+
+
+$drink = "coffee";
+$drink2 = "tea";
+
+$size = "large";
+$size2 = "medium";
+
+$budget = 10;
+
+
+console.log("Hello my friend " + $name2 + ", how is your day?")
+console.log("I am ok " + $name)
+console.log("How about you " + $name2 + "?")
+console.log("Well I need a cup of " + $drink + " to start my day.")
+console.log("Do you need a cup of " + $drink2 + "?")
+console.log("Yes please! I want a " + $size + " one.")
+console.log("Well they only serves the " + $size2 + " size.")
+console.log("That's okay, here is my " + $budget + " dollar.")
+console.log("Thank you!")
