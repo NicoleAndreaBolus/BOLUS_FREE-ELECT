dd7578126ecaaa3b4e9a6e479b02296c5dffb627
NicoleAndreaBolus <nagbolus.student@ua.edu.ph>
2025-08-29 17:32:36 +0800

    Initial commit

---
+++ session1/myWeb1.js
@@ -0,0 +1,17 @@
+// Normal Variable Declaration
+$name = "Colin";
+console.log($name);
+
+// Using 'var'
+var $lname = "Vinuya";
+console.log($lname);
+
+// Using 'let'
+let $mySal = 25000;
+console.log($mySal);
+
+// ------ Output ------
+console.log("Hello, BSIT 3B!");
+console.log("Welcome to our JavaScript Class!");
+
+
